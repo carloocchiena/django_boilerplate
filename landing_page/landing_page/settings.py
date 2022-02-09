c624a9afa9c16f0c67a135bd63d99f9692f48fed
Carlo Occhiena <robotfood@virgilio.it>
2022-02-09 13:09:22 +0100

    update settings

--- landing_page/landing_page/settings.py
+++ landing_page/landing_page/settings.py
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# We want to search also in root folder not just app folder
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
