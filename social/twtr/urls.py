f6e66a90d3efe65f34d9404e27ec09ce0755d4ca
Carlo Occhiena <robotfood@virgilio.it>
2022-02-28 16:16:49 +0100

    fix

--- social/twtr/urls.py
+++ social/twtr/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import dashboard
+from .views import dashboard, profile_list, profile
 
 app_name = 'twtr'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('profile_list/', profile_list, name='profile_list'),
+    path('profile/<int:pk>', profile, name='profile'),
 ]
--- social/twtr/views.py
+++ social/twtr/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "twtr/base.html")
+    return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "twtr/profile_list.html", {'profiles': profiles})
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})
