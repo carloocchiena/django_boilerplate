d29ca4ebb8cf2a37cebef99d125045667c0577a1
Carlo Occhiena <robotfood@virgilio.it>
2022-03-17 14:39:07 +0100

    update

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -7,5 +7,13 @@ def project_index(request):
     return render(request, 'projects/index.html', {'projects': projects})
 
 def project_details(request, pk):
+    projects = models.Project.objects.all(), #delete this line if "next" link is not working
     project = models.Project.objects.get(pk=pk)
-    return render(request, 'projects/project_details.html', {'project': project})
+    
+    #delete this line if "next" link is not working
+    context = {
+        'projects': projects,
+        'project': project
+    }
+    
+    return render(request, 'projects/project_details.html', context=context) #modify this line if "next" link is not working
