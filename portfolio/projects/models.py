2d656a20e062bd7bf3b572755e2f6718ec72bffb
Carlo Occhiena <robotfood@virgilio.it>
2022-03-21 12:44:53 +0100

    update

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
+    """model for the single project"""
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FileField(null=True, blank=True)
+    
+    def get_next(self):
+        """function to navigate thru the projects using a Next button"""
+        next = Project.objects.filter(id__gt=self.id).order_by('id').first()
+        if next:
+            return next
+        else:
+            return Project.objects.all().order_by('id').first()
        
--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -7,13 +7,5 @@ def project_index(request):
     return render(request, 'projects/index.html', {'projects': projects})
 
 def project_details(request, pk):
-    projects = models.Project.objects.all(), #delete this line if "next" link is not working
     project = models.Project.objects.get(pk=pk)
-    
-    #delete this line if "next" link is not working
-    context = {
-        'projects': projects,
-        'project': project
-    }
-    
-    return render(request, 'projects/project_details.html', context=context) #modify this line if "next" link is not working
+    return render(request, 'projects/project_details.html', {'project': project}) 
